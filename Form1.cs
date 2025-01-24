f0656d4dc634b8fb698b63c571b7a9719be3ffae
tyou hyou <zb_116@hotmail.com>
2025-01-24 20:32:59 +0900

    1st commit

---
+++ Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WebView2Sample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var browser = new MyBrowser();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myBrowser1.Initialize();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myBrowser1.Navigate("http://google.com");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myBrowser1.Initialize();
+            myBrowser1.Navigate("http://google.com");
+            //Thread.Sleep(5000); // main thread blocking
+            myBrowser1.Url = new Uri("http://bing.com");
+        }
+    }
+}
---
+++ MyBrowser.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Security.Policy;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebView2Sample
+{
+    internal class MyBrowser : WebView2
+    {
+        private enum InitStatus
+        {
+            UnInit = 0,
+            Initing,
+            Inited
+        }
+
+        InitStatus status { set; get; } = InitStatus.UnInit;
+
+        private Task task { set; get; } = null;
+
+        public void Initialize()
+        {
+            status = InitStatus.Initing;
+            CoreWebView2InitializationCompleted += MyBrowser_CoreWebView2InitializationCompleted;
+            SynchronizationContext.Current.Post((_) => 
+            {
+                task = EnsureCoreWebView2Async();
+            }, null);
+        }
+
+        private void MyBrowser_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            CoreWebView2.DOMContentLoaded += (o, args) => { };
+            status = InitStatus.Inited;
+        }
+
+        public void Navigate(string url)
+        {
+            Await(()=> _Navigate(url));
+        }
+        private void _Navigate(string url)
+        {
+            CoreWebView2.Navigate(url);
+        }
+
+        public Uri Url
+        {
+            set => Await(() => Source = value);
+            get => Source;
+        }
+
+        private void Await(Action act)
+        {
+            if (InitStatus.UnInit == status)
+            {
+                throw new InvalidOperationException("Not initialized yet.");
+            }
+
+            if (InitStatus.Inited == status)
+            {
+                act();
+                return;
+            }
+
+            if (null == SynchronizationContext.Current) throw new InvalidOperationException("Should be on UI thread");
+            SynchronizationContext.Current.Post(async (_) => 
+            {
+                await task;
+                if (!task.IsCompleted) return;  // TODO: log
+                act(); 
+            }, null);
+        }
+    }
+}
